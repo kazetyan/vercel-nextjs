d0ebe82f48901b1ee352f9126c27b61f0e2362a3
wutong <710893846@qq.com>
2022-09-14 11:16:32 +0800

    add toArray()

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -8,7 +8,7 @@ const queryTest = async function queryTest() {
         const tests = database.collection('test');
         // Query for a movie that has the title 'Back to the Future'
         const query = {};
-        const test = await tests.find(query);
+        const test = await tests.find(query).toArray();
         console.log(test)
         return test
     } catch (ex) {
